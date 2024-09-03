1b068eac8abeb138348a2b9284bb3b56452b974e
amalkhairin <amallanch45@gmail.com>
2024-09-03 14:26:52 +0700

    add home stack

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <View style={styles.container}>
-          <Text>Hello world</Text>
-          <StatusBar style="auto" />
-        </View>
-      </NavigationContainer>
-    </SafeAreaProvider>
-  );
+    return (
+        <SafeAreaProvider>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Home" component={() => {
+                        return <View style={styles.container}>
+                            <Text>Hello world</Text>
+                            <StatusBar style="auto" />
+                        </View>
+                    }} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </SafeAreaProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
