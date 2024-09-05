e09f1b288c384dfa9d51453e6bf5e1cadc1e4f57
amalkhairin <amallanch45@gmail.com>
2024-09-05 13:19:35 +0700

    refactoring code

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ if (__DEV__) {
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import 'react-native-gesture-handler';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
@@ -13,12 +16,12 @@ const BottomTab = createMaterialBottomTabNavigator();
 const TopTab = createMaterialTopTabNavigator();
 
 const DummyComponent = (name) => () => {
-    return <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>{name}</Text>
     </View>
 }
 
-const BottomTabScreens = () => {
+const TopTabScreens = () => {
     return (
         <TopTab.Navigator>
             <TopTab.Screen name="Profile" component={DummyComponent('Profile')} />
@@ -27,16 +30,61 @@ const BottomTabScreens = () => {
     )
 }
 
+const homeOptions = {
+    tabBarIcon: ({ color }) => {
+        return <Ionicons name="home" size={24} color={color} />
+    }
+}
+const productOptions = {
+    tabBarIcon: ({ color }) => {
+        return <Ionicons name="cube" size={24} color={color} />
+    }
+}
+const cartOptions = {
+    tabBarIcon: ({ color }) => {
+        return <Ionicons name="cart" size={24} color={color} />
+    }
+}
+const settingOptions = {
+    tabBarIcon: ({ color }) => {
+        return <Ionicons name="cog" size={24} color={color} />
+    }
+}
+
+const Stack = createNativeStackNavigator()
+
+const PublicNavigator = () => {
+    return (
+        <Stack.Navigator initialRouteName='Login'>
+            <Stack.Screen name="Login" component={DummyComponent('Login')} />
+            <Stack.Screen name="Register" component={DummyComponent('Register')} />
+        </Stack.Navigator>
+    )
+}
+
+const ProtectedNavigator = () => {
+    return (
+        <BottomTab.Navigator >
+            <BottomTab.Screen name="Home" options={homeOptions} component={DummyComponent('Home')} />
+            <BottomTab.Screen name="Profile" options={productOptions} component={DummyComponent('Product')} />
+            <BottomTab.Screen name="Cart" options={cartOptions} component={DummyComponent('Cart')} />
+            <BottomTab.Screen name="Setting" options={settingOptions} component={TopTabScreens} />
+        </BottomTab.Navigator>
+    )
+}
+
 export default function App() {
+
+    const [isLogin, setIsLogin] = useState(false);
+
     return (
         <SafeAreaProvider>
             <SafeAreaView style={{ flex: 1 }}>
                 <NavigationContainer>
-                    <BottomTab.Navigator>
-                        <BottomTab.Screen name="Home" component={DummyComponent('Home')} />
-                        <BottomTab.Screen name="Profile" component={DummyComponent('Product')} />
-                        <BottomTab.Screen name="Setting" component={BottomTabScreens} />
-                    </BottomTab.Navigator>
+                    {isLogin ?
+                        <ProtectedNavigator />
+                        : <PublicNavigator />
+                    }
                 </NavigationContainer>
             </SafeAreaView>
         </SafeAreaProvider>
